02a3d0619cac349f94203b4034de66f7517bcc5c
devchristines <dev.christines@gmail.com>
2025-10-09 15:40:13 +0800

    update about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,21 +1,37 @@
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
 
 
 const About = () => {
     return (
         <Container id="about" className="section">
             <h1>About Me</h1>
-            <ul>
-                <li><h3>Frontend</h3></li>
-                <li>React JS</li>
-                <li>Material UI</li>
-            </ul>
-            <ul>
-                <li><h3>Backend</h3></li>
-                <li>PHP</li>
-                <li>Python</li>
-            </ul>
+            <Grid container spacing={2}>
+                <Grid size={3}>
+                    <Grid size={12}><h3>Frontend</h3></Grid>
+                    <Grid size={12}>React</Grid>
+                    <Grid size={12}>Material UI</Grid>
+                </Grid>
+                <Grid size={3}>
+                    <Grid size={12}><h3>Backend</h3></Grid>
+                    <Grid size={12}>PHP</Grid>
+                </Grid>
+                <Grid size={3}>
+                    <Grid size={12}><h3>Architecture</h3></Grid>
+                    <Grid size={12}>AWS - ECS, EC2, RDS, SQS</Grid>
+                </Grid>
+                <Grid size={3}>
+                    <Grid size={12}><h3>CMS</h3></Grid>
+                    <Grid size={12}>Wordpress</Grid>
+                </Grid>
+                <Grid size={12}>
+                    <Grid size={12}><h3>Companies I've Worked With</h3></Grid>
+                    <Grid size={12}>Surefire Local</Grid>
+                    <Grid size={12}>Lamudi</Grid>
+                </Grid>
+            </Grid>
+
         </Container>
     )
 }
